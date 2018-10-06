2e367b014b99ed96093b9a5ca6a570d8758222e0
Suryakiran Suresh Gurumoorthy Iyer <ec2-user@ip-172-31-25-74.us-west-2.compute.internal>
2018-10-06 09:11:39 +0000

    Part 2 complete with heroku deployment

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -63,7 +63,12 @@ class MoviesController < ApplicationController
   
   def same_director
     @movie = Movie.find(params[:id])
+    if @movie.director.nil? || @movie.director.empty?
+      flash[:notice] = "'#{@movie.title}' has no director info"
+      redirect_to movies_path, alert: "'#{params[:title]}' has no director info"
+    end
     @movies = Movie.filter_by_director(@movie.director)
+    
   end
 
 end
